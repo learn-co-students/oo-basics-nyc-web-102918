cc9dead3a732d6c1edc5878d13fad30b41f023a2
Connor Callahan <connorwcallahan@gmail.com>
2018-10-30 17:27:01 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,9 +8,8 @@ end
 
 attr_reader :brand
 
-attr_writer :color, :size, :material, :condition
+attr_accessor :color, :size, :material, :condition
 
-attr_reader :color, :size, :material, :condition
 
 def cobble
   puts "Your shoe is as good as new!"
