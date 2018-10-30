57ed61f50ed9457a06d2cb1dda63d13371859168
liuh <hanke.liu@gmail.com>
2018-10-30 17:25:33 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,43 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  # def title
+  #   @title
+  # end
 
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
+  # def author=(author)
+  #   @author = author
+  # end
+  #
+  # def author
+  #   @author
+  # end
+  #
+  # def page_count=(num)
+  #   @page = num
+  # end
+  #
+  # def page_count
+  #   @page
+  # end
+  #
+  # def genre=(genre)
+  #   @genre = genre
+  # end
+  #
+  # def genre
+  #   @genre
+  # end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  # book = Book.new("Some Title")
+  # book.turn_page
+end
